1e0f76e45939a0962ac6bd330fb479d7e7b7ac84
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-28 20:14:42 +0300

    Add payment buttons to PaymentView

---
+++ src/views/PaymentView.vue
@@ -0,0 +1,61 @@
+<script setup>
+</script>
+
+
+<template>
+    <h1>Выберите карту</h1>
+    <div class="rel">
+        <div class="bill-block">
+            <div class="bill-text">ИТОГ:</div>
+            <div class="bill-text">1000 ₽</div>
+        </div>
+        <RouterLink to="success" class="success">ОПЛАТИТЬ</RouterLink>
+        <RouterLink to="cancel" class="cancel">ОТМЕНА</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+
+div.bill-block {
+    text-align: justify;
+    display: flex;
+    justify-content:space-between;
+}
+
+div.bill-text {
+    font-weight: bold;
+    font-size: x-large;
+    display: inline;
+}
+
+a {
+    margin-block: 1em;
+}
+a.success {
+    text-decoration: none;
+    background-color: #21BE1D;
+    color: aliceblue;
+    padding-inline: 10rem;
+    padding-block: 1rem;
+    margin-top: 10px;
+    border-radius: 1em;
+    font-weight: bold;
+}
+
+a.cancel {
+    text-decoration: none;
+    color: #21BE1D;
+    font-weight: bold;
+}
+
+div.rel {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    margin-bottom: 1em;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+}
+</style>
