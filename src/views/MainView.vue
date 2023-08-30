e28c3a14603f89fba7fe5ffc068fc70ae8f5d427
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-30 09:50:04 +0300

    Add main loading view

---
+++ src/views/MainView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import IconCentrInvest from '../components/icons/IconCentrInvest.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const onTap = () => {
+    router.push({
+        name: 'login',
+      })
+}
+
+</script>
+
+<template>
+    <div class="full" v-touch:tap="onTap">
+        <IconCentrInvest />
+        <h3>Коснитесь экрана для начала</h3>
+        <h3>работы терминала</h3>
+    </div>
+</template>
+
+<style scoped>
+h3 {
+    font-size: 1.2rem;
+}
+
+div.full {
+    width: 100%;
+    height: 100%;
+    background: red;
+}
+</style>
