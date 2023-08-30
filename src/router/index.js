379285ba4335715bd7db801e82053b2144f37480
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-30 09:48:25 +0300

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainView from '../views/MainView.vue'
 import LoginView from '../views/LoginView.vue'
 import PaymentView from '../views/PaymentView.vue'
+import SuccessView from '../views/SuccessView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'main',
+      component: MainView
+    },
+    {
+      path: '/login',
       name: 'login',
       component: LoginView
     },
@@ -14,6 +21,11 @@ const router = createRouter({
       path: '/payment',
       name: 'payment',
       component: PaymentView
+    },
+    {
+      path: '/success',
+      name: 'success',
+      component: SuccessView
     },
   ]
 })
